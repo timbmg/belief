fae9999458c9b93cfea4f4eb5cb8db4bd98d32f1
timbmg <baumgaertner.t@gmail.com>
2018-09-05 14:19:29 +0200

    dataset download

---
+++ guesswhat/data/download.sh
@@ -0,0 +1,5 @@
+cd guesswhat/data
+
+curl -O https://s3-us-west-2.amazonaws.com/guess-what/guesswhat.train.jsonl.gz
+curl -O https://s3-us-west-2.amazonaws.com/guess-what/guesswhat.valid.jsonl.gz
+curl -O https://s3-us-west-2.amazonaws.com/guess-what/guesswhat.test.jsonl.gz
